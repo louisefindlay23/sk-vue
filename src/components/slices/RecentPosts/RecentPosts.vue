1b634e9db7c10c5df0740600f530d285adf2c5bd
Louise Findlay <louise.findlay@prismic.io>
2023-05-10 14:08:09 +0100

    Limited recent post list

--- src/components/slices/RecentPosts/RecentPosts.vue
+++ src/components/slices/RecentPosts/RecentPosts.vue
@@ -2,15 +2,17 @@
 import { getSliceComponentProps } from "@prismicio/vue";
 // The array passed to `getSliceComponentProps` is purely optional and acts as a visual hint for you
 defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]));
-import { useAllPrismicDocumentsByType } from "@prismicio/vue";
-const { data: posts } = useAllPrismicDocumentsByType("posts");
+import { usePrismicDocumentsByType } from "@prismicio/vue";
+const { data: posts } = usePrismicDocumentsByType("posts", {
+  pageSize: 3,
+});
 import Date from "../../../components/Date/Date.vue";
 </script>
 
 <template>
   <div id="box-container">
     <PrismicRichText :field="slice.primary.post_heading" wrapper="section" />
-    <article v-for="post in posts" :key="JSON.stringify(post.data)">
+    <article v-for="post in posts.results" :key="JSON.stringify(post.data)">
       <div class="box-content">
         <a :href="`/post/${post.uid}`">
           <PrismicRichText :field="post.data.post_title" />
