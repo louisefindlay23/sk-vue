6c232c9078083112079ca2375acc70ca62979789
Louise Findlay <louise.findlay@prismic.io>
2023-05-11 16:28:27 +0100

    Fixed Date for recent posts

--- src/components/Date/Date.vue
+++ src/components/Date/Date.vue
@@ -6,9 +6,12 @@ import { useRouter } from "vue-router";
 const route = useRouter();
 const uid = route.currentRoute.value.params.uid;
 const { data: post } = usePrismicDocumentByUID("posts", uid);
+defineProps(["postDate"]);
 </script>
 <template>
-  <time :dateTime="prismicH.asDate(post.first_publication_date).toISOString()">
+  <time v-if="post" :dateTime="prismicH.asDate(post.first_publication_date).toISOString()">
     {{ format(prismicH.asDate(post.first_publication_date), "dd/MM/yyyy") }}
   </time>
+  <time v-if="postDate" :dateTime="prismicH.asDate(postDate)">
+    {{ format(prismicH.asDate(postDate), "dd/MM/yyyy") }}</time>
 </template>
