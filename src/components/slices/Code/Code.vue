3be134fff3110c3e4b29e2fbc967acce2631f7ec
Louise Findlay <louise.findlay@prismic.io>
2023-05-11 16:39:54 +0100

    Fixed code html serializer

--- src/components/slices/Code/Code.vue
+++ src/components/slices/Code/Code.vue
@@ -3,12 +3,11 @@ import { getSliceComponentProps } from "@prismicio/vue";
 
 // The array passed to `getSliceComponentProps` is purely optional and acts as a visual hint for you
 defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]));
-const htmlSerializer = {
-  label: ({ node, children }) =>
-    `<code label=${node.data.label}>${children}</code>`,
+const codeSerializer = {
+  preformatted: ({ children }) => `<pre class="code">${children}</pre>`,
 };
 </script>
 
 <template>
-  <PrismicRichText :field="slice.primary.code" :components="htmlSerializer" />
+  <PrismicRichText :field="slice.primary.code" :htmlSerializer="codeSerializer" />
 </template>
